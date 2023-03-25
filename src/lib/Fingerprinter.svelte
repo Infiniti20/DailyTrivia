b4fb0f168c99f049a1e0c3c0f19bbc7ba7f137ed
Kieran Roy <kierankroy2010@gmail.com>
2023-03-25 13:45:22 -0400

    bug: added canvas hash to fingerprint

--- src/lib/Fingerprinter.svelte
+++ src/lib/Fingerprinter.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let fingerprint;
-  function getFingerprint():number {
+  function getFingerprint(): number {
     function createFingerprint(string: string) {
       var hash = 0,
         i,
@@ -15,37 +15,60 @@
     }
 
     var canvas = document.createElement("canvas");
+    var canvas2 = document.createElement("canvas");
     var gl = canvas.getContext("webgl");
+
+    var ctx = canvas2.getContext("2d");
+    if (ctx == null) {
+      return 0;
+    }
+    if (gl == null) {
+      return 0;
+    }
+
+    // https://www.browserleaks.com/canvas#how-does-it-work
+    var txt = "CANVAS_FINGERPRINT";
+    ctx.textBaseline = "top";
+    ctx.font = "14px 'Arial'";
+    ctx.textBaseline = "alphabetic";
+    ctx.fillStyle = "#f60";
+    ctx.fillRect(125, 1, 62, 20);
+    ctx.fillStyle = "#069";
+    ctx.fillText(txt, 2, 15);
+    ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
+    ctx.fillText(txt, 4, 17);
+    let dataURL = canvas2.toDataURL();
 
     var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     var timezone_offset = new Date().getTimezoneOffset();
-    if(gl == null){return 0}
     var graphics_card = gl.getParameter(gl.RENDERER);
 
     return createFingerprint(
-        window.screen.availWidth +
-          "x" +
-          window.screen.availHeight +
-          "_" +
-          window.screen.colorDepth +
-          "_" +
-          navigator.userAgent +
-          "_" +
-          navigator.platform +
-          "_" +
-          navigator.language +
-          "_" +
-          navigator.hardwareConcurrency +
-          "_" +
-          navigator.cookieEnabled +
-          "_" +
-          timezone +
-          "_" +
-          timezone_offset +
-          "_" +
-          graphics_card
-      )
+      window.screen.availWidth +
+        "x" +
+        window.screen.availHeight +
+        "_" +
+        window.screen.colorDepth +
+        "_" +
+        navigator.userAgent +
+        "_" +
+        navigator.platform +
+        "_" +
+        navigator.language +
+        "_" +
+        navigator.hardwareConcurrency +
+        "_" +
+        navigator.cookieEnabled +
+        "_" +
+        timezone +
+        "_" +
+        timezone_offset +
+        "_" +
+        graphics_card +
+        "_" +
+        dataURL
+    );
   }
 
-  fingerprint = getFingerprint()
+  fingerprint = getFingerprint();
 </script>
